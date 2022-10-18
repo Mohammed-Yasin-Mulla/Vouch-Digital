9b4cec387f36163d904a29b823ae88846894c304
Mohammed Yasin Mulla <yasin@theflurn.com>
2022-10-18 08:36:52 +0530

    feat: installed antd package and init setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,62 @@
 import '../styles/globals.css'
+import "antd/dist/antd.css";
 import type { AppProps } from 'next/app'
 
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
+import React, { useState } from 'react';
+const { Header, Sider, Content } = Layout;
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+      <Layout className="layout">
+        <Sider>
+          <div className="logo" />
+          <Menu
+              theme="dark"
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              items={[
+                {
+                  key: '1',
+                  icon: <UserOutlined />,
+                  label: 'nav 1',
+                },
+                {
+                  key: '2',
+                  icon: <VideoCameraOutlined />,
+                  label: 'nav 2',
+                },
+                {
+                  key: '3',
+                  icon: <UploadOutlined />,
+                  label: 'nav 3',
+                },
+              ]}
+          />
+        </Sider>
+        <Layout className="site-layout">
+
+          <Content
+              className="site-layout-background"
+              style={{
+                margin: '24px 16px',
+                padding: 24,
+              }}
+          >
+           <Component {...pageProps} />
+          </Content>
+        </Layout>
+      </Layout>
+  )
+
 }
 
 export default MyApp
