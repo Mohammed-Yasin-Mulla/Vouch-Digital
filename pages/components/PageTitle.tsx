c5a002abf7eecfd72b75922cf7d4c9a63a0d23ee
Mohammed Yasin Mulla <yasin@theflurn.com>
2022-10-18 17:51:55 +0530

    refactor: added bell icon on the page tile comp

--- pages/components/PageTitle.tsx
+++ pages/components/PageTitle.tsx
@@ -1,4 +1,5 @@
-import { Breadcrumb, Typography } from "antd";
+import { BellOutlined } from "@ant-design/icons";
+import { Breadcrumb, Button, Typography } from "antd";
 import React from "react";
 import styled from "styled-components";
 
@@ -10,18 +11,28 @@ interface PageTitleProps {
 export default function PageTitle(props: PageTitleProps) {
   const { title, route } = props;
   return (
-    <>
-      <StyledTitle>{title}</StyledTitle>
-      <Breadcrumb>
-        {route?.map((item) => (
-          <Breadcrumb.Item key={item}>{item}</Breadcrumb.Item>
-        ))}
-      </Breadcrumb>
-    </>
+    <Wrapper>
+      <div>
+        <StyledTitle>{title}</StyledTitle>
+        <Breadcrumb>
+          {route?.map((item) => (
+            <Breadcrumb.Item key={item}>{item}</Breadcrumb.Item>
+          ))}
+        </Breadcrumb>
+      </div>
+      <Button type="link" icon={<BellOutlined />} style={{color: 'black'}} />
+    </Wrapper>
   );
 }
 
 const StyledTitle = styled(Typography.Text)`
   font-size: 24px;
   font-weight: 700;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+align-items: flex-start;
+
 `;
