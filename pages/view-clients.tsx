846a40f0b3b8babfabfc8f4bdbbe3b0f32b8f8a1
Mohammed Yasin Mulla <yasin@theflurn.com>
2022-10-18 18:37:29 +0530

    feat: added search and button in view-client page

--- pages/view-clients.tsx
+++ pages/view-clients.tsx
@@ -1,8 +1,15 @@
-import { Table } from "antd";
+import { Button, Dropdown, Menu, Space, Table } from "antd";
 import React from "react";
 import PageTitle from "./components/PageTitle";
+import { StyledSearchBar } from "./components/StyledSearchBar";
 import { columns } from "./components/ViewClient/columns";
 import { dummyData } from "./components/ViewClient/dummyData";
+import {
+  SearchOutlined,
+  DownOutlined,
+  DownloadOutlined,
+} from "@ant-design/icons";
+import styled from "styled-components";
 
 export interface TableData {
   companyName: string;
@@ -15,6 +22,25 @@ export interface TableData {
   teenageGroups?: number;
 }
 
+const menu = (
+  <Menu
+    items={[
+      {
+        label: "Company Name",
+        key: "1",
+      },
+      {
+        label: "Email Address",
+        key: "2",
+      },
+      {
+        label: "Phone No",
+        key: "3",
+      },
+    ]}
+  />
+);
+
 export default function ViewClients() {
   return (
     <>
@@ -22,6 +48,26 @@ export default function ViewClients() {
         title="View Clients"
         route={["Client Master", "View Client"]}
       />
+      <Wrapper>
+        <StyledSearchBar
+          placeholder="Search"
+          width={300}
+          addonBefore={<SearchOutlined />}
+        />
+        <ButtonWrapper>
+          <Dropdown overlay={menu}>
+            <Button>
+              <Space>
+                Columns
+                <DownOutlined />
+              </Space>
+            </Button>
+          </Dropdown>
+          <Button type="primary" icon={<DownloadOutlined />}>
+            Export
+          </Button>
+        </ButtonWrapper>
+      </Wrapper>
       <Table
         columns={columns}
         dataSource={dummyData}
@@ -30,3 +76,17 @@ export default function ViewClients() {
     </>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex !important;
+  margin-top: 40px;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
