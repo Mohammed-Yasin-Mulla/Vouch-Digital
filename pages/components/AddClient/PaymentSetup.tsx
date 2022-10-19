6a4df3d342f77103430a333ad21e1669237298cb
Mohammed Yasin Mulla <yasin@theflurn.com>
2022-10-19 14:23:35 +0530

    feat: added payentSetupComp

--- pages/components/AddClient/PaymentSetup.tsx
+++ pages/components/AddClient/PaymentSetup.tsx
@@ -1,7 +1,178 @@
-import React from 'react'
+import { Checkbox, Form, Radio } from "antd";
+import React from "react";
+import styled from "styled-components";
+import FormInput from "../FormComp/FormInput";
+import FormSelect from "../FormComp/FormSelect";
+import SaveAndContinue from "../SaveAndContinue";
+import TabTitle from "../TabTitle";
+
+const CheckboxItem = [
+  {
+    value: "upi",
+    label: "UPI",
+  },
+  {
+    value: "net_banking",
+    label: "Net Banking",
+  },
+  {
+    value: "card",
+    label: "Card",
+  },
+  {
+    value: "check",
+    label: "Check",
+  },
+  {
+    value: "other",
+    label: "Other",
+  },
+];
+
+const RadioItem = [
+  {
+    value: "paid",
+    label: "Paid",
+  },
+  {
+    value: "Pending",
+    label: "Pending",
+  },
+];
 
 export default function PaymentSetup() {
+  const [form] = Form.useForm();
+
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
   return (
-    <div>PaymentSetup</div>
-  )
+    <>
+      <TabTitle
+        title="Payment Setup"
+        description="Setup Payments for the clients"
+      />
+      <Form form={form} onFinish={onFinish} onFinishFailed={onFinishFailed} style={{marginTop:'46px'}}>
+        <FormInput
+          name="billing_address"
+          placeHolder="Billing Address"
+          rule="Please enter the billing address"
+        />
+        <Wrapper>
+          <div>
+            <FormSelect
+              name="state"
+              placeHolder="Select State"
+              rule="Please Select the state"
+              options={[
+                {
+                  value: "1",
+                  label: "State 1",
+                },
+              ]}
+            />
+            <FormSelect
+              name="site_access"
+              placeHolder="Select Number of Site Access"
+              rule="Please Select the site access"
+              options={[
+                {
+                  value: "1",
+                  label: "State 1",
+                },
+              ]}
+            />
+            <FormInput
+              name="discount"
+              placeHolder="Discount(%)"
+              rule="Please Enter the discount"
+            />
+            <Form.Item name="checkbox-group" label="Checkbox.Group">
+              <Checkbox.Group>
+                {CheckboxItem.map((item, index) => {
+                  return (
+                    <Checkbox value={item.value} key={index}>
+                      {item.label}
+                    </Checkbox>
+                  );
+                })}
+              </Checkbox.Group>
+            </Form.Item>
+          </div>
+          {/*  */}
+          <div>
+            <FormWrapper>
+              <FormSelect
+                name="city"
+                placeHolder="Select City"
+                options={[
+                  {
+                    value: "1",
+                    label: "City 1",
+                  },
+                ]}
+                rule="Please Select the City"
+              />
+              <FormInput
+                name="pin_code"
+                placeHolder="Pincode"
+                width="40%"
+                rule="Please Enter the pin code"
+              />
+            </FormWrapper>
+              <FormInput
+                prefix="Rs."
+                suffix={"Total Rs.0000"}
+                name="amount_per_site"
+                placeHolder="Payable Amount per site"
+                rule="Please Enter payable amount per site"
+              />
+              <FormInput
+                name="reason"
+                placeHolder="Reason"
+                rule="Please enter a Reason"
+              />
+            <Form.Item name="payment_status" label="Payment Status">
+              <Radio.Group>
+                {RadioItem.map((item, index) => {
+                  return (
+                    <Radio value={item.value} key={index}>
+                      {item.label}
+                    </Radio>
+                  );
+                })}
+              </Radio.Group>
+            </Form.Item>
+            <div />
+          </div>
+        </Wrapper>
+      </Form>
+
+      <SaveAndContinue
+        saveAndContinue={() => form.submit()}
+        reset={() => form.resetFields()}
+      />
+    </>
+  );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  margin-top: 20px;
+  justify-content: space-between;
+  gap: 16px;
+  & > div {
+    width: 50%;
+  }
+`;
+
+const FormWrapper = styled.div`
+  width: 100%;
+  gap: 8px;
+  display: flex;
+  justify-content: flex-start;
+`;
--- pages/components/FormComp/FormInput.tsx
+++ pages/components/FormComp/FormInput.tsx
@@ -7,16 +7,18 @@ interface FormInputProps {
   name: string;
   placeHolder?: string;
   rule?: string;
-  width?: string
+  width?: string;
+  prefix?: React.ReactNode;
+  suffix?: React.ReactNode;
 }
 
 export default function FormInput(props: FormInputProps) {
-  const { label, name, rule, placeHolder , width} = props;
+  const { label, name, rule, placeHolder, width, prefix, suffix } = props;
   return (
     <Form.Item
       label={label}
       name={name}
-      style={{width: width ? width : "100%"}}
+      style={{ width: width ? width : "100%" }}
       rules={[
         {
           required: rule !== undefined && rule !== "" ? true : false,
@@ -25,7 +27,9 @@ export default function FormInput(props: FormInputProps) {
       ]}
     >
       <StyledInput
-      width={width}
+        prefix={prefix}
+        suffix={suffix}
+        width={width}
         placeholder={`${placeHolder} ${
           rule !== undefined && rule !== "" ? "*" : ""
         }`}
@@ -34,10 +38,9 @@ export default function FormInput(props: FormInputProps) {
   );
 }
 
-
 const StyledInput = styled(Input)`
   border-radius: 5px;
   border: 1px solid #e8e8e8;
   padding: 12 16px;
   width: 100%;
-  `
+`;
