056b8ca1d1b8886d4d27123f49bff0e269f402da
Mohammed Yasin Mulla <yasin@theflurn.com>
2022-10-19 18:44:43 +0530

    feat: created succes tab

--- pages/add-client.tsx
+++ pages/add-client.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import CreateProfile from "./components/AddClient/CreateProfile";
 import ModulesSetup from "./components/AddClient/ModulesSetup";
 import PaymentSetup from "./components/AddClient/PaymentSetup";
+import Success from "./components/AddClient/Success";
 import ThemeSetup from "./components/AddClient/ThemeSetup";
 import PageTitle from "./components/PageTitle";
 
@@ -11,6 +12,7 @@ export enum TabName {
   payment_setup = "Payment Setup",
   theme_setup = "Theme Setup",
   modules_setup = "Modules Setup",
+  success = "Success",
 }
 
 export enum TabShortName {
@@ -18,11 +20,12 @@ export enum TabShortName {
   PAYMENT_SETUP = "payment_setup",
   THEME_SETUP = "theme_setup",
   MODULES_SETUP = "modules_setup",
+  SUCCESS = "success",
 }
 
 export default function AddClient() {
   const [currentTab, setCurrentTab] = useState<keyof typeof TabName>(
-    TabShortName.CREATE_PROFILE
+    TabShortName.SUCCESS
   );
 
   const onChangeTab = (key: string) => {
@@ -35,32 +38,41 @@ export default function AddClient() {
         title={"Add Client"}
         route={["Client Master", "Add Client", TabName[currentTab]]}
       />
-      <Tabs
-        activeKey={currentTab}
-        onChange={onChangeTab}
-        items={[
-          {
-            label: TabName.create_profile,
-            key: TabShortName.CREATE_PROFILE,
-            children: <CreateProfile />,
-          },
-          {
-            label: TabName.payment_setup,
-            key: TabShortName.PAYMENT_SETUP,
-            children: <PaymentSetup />,
-          },
-          {
-            label: TabName.theme_setup,
-            key: TabShortName.THEME_SETUP,
-            children: <ThemeSetup />,
-          },
-          {
-            label: TabName.modules_setup,
-            key: TabShortName.MODULES_SETUP,
-            children: <ModulesSetup />,
-          },
-        ]}
-      />
+      {currentTab !== TabShortName.SUCCESS && (
+        <Tabs
+          activeKey={currentTab}
+          onChange={onChangeTab}
+          items={[
+            {
+              label: TabName.create_profile,
+              key: TabShortName.CREATE_PROFILE,
+              children: <CreateProfile />,
+            },
+            {
+              label: TabName.payment_setup,
+              key: TabShortName.PAYMENT_SETUP,
+              children: <PaymentSetup />,
+            },
+            {
+              label: TabName.theme_setup,
+              key: TabShortName.THEME_SETUP,
+              children: <ThemeSetup />,
+            },
+            {
+              label: TabName.modules_setup,
+              key: TabShortName.MODULES_SETUP,
+              children: <ModulesSetup />,
+            },
+            {
+              label: TabName.modules_setup,
+              key: TabShortName.MODULES_SETUP,
+              children: <ModulesSetup />,
+            },
+          ]}
+        />
+      )}
+
+      {currentTab === TabShortName.SUCCESS && <Success />}
     </>
   );
 }
---
+++ pages/components/AddClient/Success.tsx
@@ -0,0 +1,110 @@
+import React from "react";
+import { CheckCircleTwoTone } from "@ant-design/icons";
+import { UserOutlined } from "@ant-design/icons";
+import { Avatar, Button, Typography } from "antd";
+import styled from "styled-components";
+
+export default function Success() {
+  return (
+    <>
+      <TopWrapper>
+        <TextWrapper>
+          <CheckCircleTwoTone
+            twoToneColor={"#52c41a"}
+            style={{ fontSize: "32px" }}
+          />
+          <StyledTypography>
+            You have successfully added Infosys.
+          </StyledTypography>
+        </TextWrapper>
+        <ButtonWrapper>
+          <Button>Go to Client Master</Button>
+          <Button type="primary">Add More Clients</Button>
+        </ButtonWrapper>
+      </TopWrapper>
+
+      <CardWrapper>
+        {/* create empty array of length 5 */}
+        {Array(5).fill(null).map((_, index) => (
+          <Card key={index} />
+        ))}
+
+      </CardWrapper>
+    </>
+  );
+}
+
+const TopWrapper = styled.div`
+  margin-top: 23px;
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid lightgrey;
+  padding: 24px;
+  border-radius: 8px;
+  box-shadow: 0px 1px 5px rgba(3, 0, 55, 0.08);
+`;
+
+const StyledTypography = styled(Typography.Text)`
+  font-size: 18px;
+  font-weight: 700;
+`;
+
+const TextWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 12px;
+  align-items: center;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 12px;
+`;
+
+const CardWrapper = styled.div`
+  display: flex;
+  gap: 20px;
+  justify-content: flex-start;
+  margin-top: 20px;
+`;
+
+const Card = () => {
+  return (
+    <CardBody>
+      <Avatar size={64} icon={<UserOutlined />} />
+      <CardTitleText>Dummy Value</CardTitleText>
+      <CardSubText>4 Clients</CardSubText>
+      <Button type="primary" style={{ marginTop: "32px", width: "80%" }}>
+        Add FCM
+      </Button>
+      <Button style={{ marginTop: "2px" }} type="link">
+        View All
+      </Button>
+    </CardBody>
+  );
+};
+
+const CardBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 293px;
+  height: 326px;
+  border: 1px solid lightgray;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.05);
+`;
+
+const CardTitleText = styled(Typography.Text)`
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 24px;
+  margin-top: 24px;
+`;
+
+const CardSubText = styled(Typography.Text)`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  color: #686687;
+`;
